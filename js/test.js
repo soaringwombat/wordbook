d4da3ee5e90a29edb6b836060cbbae7b0a0bcf43
yosuke <yousuke.k2001@gmail.com>
2023-09-28 22:49:12 +0900

    :+1: [fix]テスト機能の調整

--- js/test.js
+++ js/test.js
@@ -1,29 +1,38 @@
 // 単語リスト
-const wordList = [
-    { id: 1, word: "apple", meaning: "りんご" },
-    { id: 2, word: "banana", meaning: "バナナ" },
-    { id: 3, word: "cherry", meaning: "さくらんぼ" },
-    { id: 4, word: "orange", meaning: "オレンジ" },
-    { id: 5, word: "grape", meaning: "ぶどう" },
-  ];
+const wordList = [];
+
+// 配列をシャッフルする関数
+function arrayShuffle(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+  }
   
-  // テストを開始するボタン
-  const startButton = document.querySelector("button");
+// テストを開始するボタンを作成する
+const button = document.createElement("button");
+button.textContent = "テストを開始する";
+document.body.appendChild(button);
   
   // テストを開始するボタンがクリックされたときの処理
   startButton.addEventListener("click", () => {
+    let numbers = [];
+    for (let i = 0; i < wordList.length; i++) {
+      numbers.push(i);
+    }
+    for (let i = 0; i < wordList.length * 2; i++) {
+      arrayShuffle(numbers);
+    }
     // ランダムに単語を選択する
-    const wordData = wordList[Math.floor(Math.random() * wordList.length)];
+    for (let i = 0; i < wordList.length; i++) {
+      const question = `「${wordList[numbers[i]].meaning}」の英語は？`;
+      const answer = wordList[numbers[i]].word;
+      const userAnswer = prompt(question);
   
-    // 問題を表示する
-    const question = `「${wordData.meaning}」の英語は？`;
-    const answer = wordData.word;
-    const userAnswer = prompt(question);
-  
-    // 答えをチェックする
-    if (userAnswer === answer) {
-      alert("正解！");
-    } else {
-      alert(`不正解！正解は「${answer}」です。`);
+      if (userAnswer === answer) {
+        alert("正解！");
+      } else {
+        alert(`不正解！正解は「${answer}」です。`);
+      }
     }
   });
