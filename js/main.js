7b99796583fa080a546f5484c08daa7df66619ed
yosuke <yousuke.k2001@gmail.com>
2023-09-29 23:11:33 +0900

    :art: [format]コードの整形

--- js/main.js
+++ js/main.js
@@ -3,30 +3,30 @@ let wordList = [];
 
 // ページが読み込まれたときに単語リストを表示する関数
 function displayWordList() {
-    const table = document.querySelector('table');
-    const tbody = table.querySelector('tbody');
-    let thead = table.querySelector('thead');
-    if (thead) {
-      table.removeChild(thead);
-    }
-    thead = table.createTHead();
-    const row = thead.insertRow();
-    const headers = ['No.', '単語', '意味'];
-  
-    for (let i = 0; i < headers.length; i++) {
-      const th = document.createElement('th');
-      const text = document.createTextNode(headers[i]);
-      th.appendChild(text);
-      row.appendChild(th);
-    }
-  
-    // すでに表示されている行をクリア
-    tbody.innerHTML = '';
-  
-    // 単語リストをテーブルに追加
-    wordList.forEach((wordData) => {
-      const row = document.createElement('tr');
-      row.innerHTML = `
+  const table = document.querySelector("table");
+  const tbody = table.querySelector("tbody");
+  let thead = table.querySelector("thead");
+  if (thead) {
+    table.removeChild(thead);
+  }
+  thead = table.createTHead();
+  const row = thead.insertRow();
+  const headers = ["No.", "単語", "意味"];
+
+  for (let i = 0; i < headers.length; i++) {
+    const th = document.createElement("th");
+    const text = document.createTextNode(headers[i]);
+    th.appendChild(text);
+    row.appendChild(th);
+  }
+
+  // すでに表示されている行をクリア
+  tbody.innerHTML = "";
+
+  // 単語リストをテーブルに追加
+  wordList.forEach((wordData) => {
+    const row = document.createElement("tr");
+    row.innerHTML = `
         <td>${wordData.id}</td>
         <td>${wordData.word}</td>
         <td>${wordData.meaning}</td>
@@ -35,76 +35,78 @@ function displayWordList() {
           <button type="button" onclick="remove(${wordData.id})">削除</button>
         </td>
       `;
-      tbody.appendChild(row);
-    });
+    tbody.appendChild(row);
+  });
 }
 
 // 単語リストをCookieに保存する関数
 function saveWordListToCookie() {
-    const json = JSON.stringify(wordList);
-    const expires = new Date(Date.now() + 60 * 60 * 1000).toUTCString(); // 有効期限を1時間に設定
-    document.cookie = `wordList=${json}; expires=${expires}; path=/`;
-  }
-  
+  const json = JSON.stringify(wordList);
+  const expires = new Date(Date.now() + 60 * 60 * 1000).toUTCString(); // 有効期限を1時間に設定
+  document.cookie = `wordList=${json}; expires=${expires}; path=/`;
+}
+
 // 単語リストを復元する関数
 function restoreWordListFromCookie() {
-  const cookie = document.cookie.split(';').find((cookie) => cookie.trim().startsWith('wordList='));
+  const cookie = document.cookie
+    .split(";")
+    .find((cookie) => cookie.trim().startsWith("wordList="));
   let restoredWordList = [];
   if (cookie) {
-    const json = cookie.split('=')[1];
+    const json = cookie.split("=")[1];
     restoredWordList = JSON.parse(json);
   }
   // 例を追加
-  else{
+  else {
     restoredWordList = [
-        { id: 1, word: 'apple', meaning: 'りんご' },
-        { id: 2, word: 'banana', meaning: 'バナナ' },
-        { id: 3, word: 'cherry', meaning: 'さくらんぼ' }
-    ]
+      { id: 1, word: "apple", meaning: "りんご" },
+      { id: 2, word: "banana", meaning: "バナナ" },
+      { id: 3, word: "cherry", meaning: "さくらんぼ" },
+    ];
   }
   wordList = restoredWordList;
   saveWordListToCookie();
   displayWordList(); // 単語リストを再表示
 }
-  
+
 // 追加ボタンがクリックされたときに単語を追加
 function addWord() {
-    const newWord = prompt('新しい単語を入力してください:');
-    const newMeaning = prompt('その単語の意味を入力してください:');
+  const newWord = prompt("新しい単語を入力してください:");
+  const newMeaning = prompt("その単語の意味を入力してください:");
 
-    if (newWord && newMeaning) {
-        const newId = wordList.length + 1;
-        wordList.push({ id: newId, word: newWord, meaning: newMeaning });
-    }
-    saveWordListToCookie();
-    displayWordList(); // 単語リストを再表示
+  if (newWord && newMeaning) {
+    const newId = wordList.length + 1;
+    wordList.push({ id: newId, word: newWord, meaning: newMeaning });
+  }
+  saveWordListToCookie();
+  displayWordList(); // 単語リストを再表示
 }
-  
+
 // 編集ボタンがクリックされたときに単語を編集
 function edit(number) {
-    const word = prompt('新しい単語を入力してください');
-    const meaning = prompt('新しい意味を入力してください');
-    if (word && meaning) {
-        wordList[number - 1].word = word;
-        wordList[number - 1].meaning = meaning;
-    }
-    saveWordListToCookie();
-    displayWordList();
+  const word = prompt("新しい単語を入力してください");
+  const meaning = prompt("新しい意味を入力してください");
+  if (word && meaning) {
+    wordList[number - 1].word = word;
+    wordList[number - 1].meaning = meaning;
+  }
+  saveWordListToCookie();
+  displayWordList();
 }
-  
+
 // 削除ボタンがクリックされたときに単語を削除
 function remove(id) {
-    const index = wordList.findIndex((wordData) => wordData.id === id);
-    if (index !== -1) {
-        wordList.splice(index, 1);
+  const index = wordList.findIndex((wordData) => wordData.id === id);
+  if (index !== -1) {
+    wordList.splice(index, 1);
 
-        // idを再度振り直す
-        for (let i = index; i < wordList.length; i++) {
-            wordList[i].id = i + 1;
-        }
-        saveWordListToCookie();
+    // idを再度振り直す
+    for (let i = index; i < wordList.length; i++) {
+      wordList[i].id = i + 1;
     }
-    displayWordList(); // 単語リストを再表示
+    saveWordListToCookie();
+  }
+  displayWordList(); // 単語リストを再表示
 }
 
 displayWordList(); // ページが読み込まれたときに単語リストを表示
