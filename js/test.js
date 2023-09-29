9dc450078da196b90e313bca02851a4464ac8c2e
yosuke <yousuke.k2001@gmail.com>
2023-09-29 23:01:41 +0900

    :bug: [fix]テスト機能にcookieに保存された単語が反映されないバグを修正

--- js/test.js
+++ js/test.js
@@ -1,5 +1,24 @@
 // 単語リスト
-const wordList = [];
+let wordList = [];
+
+// 単語リストを復元する関数
+function restoreWordListFromCookie() {
+    const cookie = document.cookie.split(';').find((cookie) => cookie.trim().startsWith('wordList='));
+    let restoredWordList = [];
+    if (cookie) {
+      const json = cookie.split('=')[1];
+      restoredWordList = JSON.parse(json);
+    }
+    // 例を追加
+    else{
+      restoredWordList = [
+          { id: 1, word: 'apple', meaning: 'りんご' },
+          { id: 2, word: 'banana', meaning: 'バナナ' },
+          { id: 3, word: 'cherry', meaning: 'さくらんぼ' }
+      ]
+    }
+    wordList = restoredWordList;
+  }
 
 // 配列をシャッフルする関数
 function arrayShuffle(array) {
@@ -11,6 +30,7 @@ function arrayShuffle(array) {
   
   // テストを開始する
   function startTest(){
+    restoreWordListFromCookie();
     let numbers = [];
     for (let i = 0; i < wordList.length; i++) {
       numbers.push(i);
